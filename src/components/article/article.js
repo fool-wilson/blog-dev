ea119a5e8fc3a0ae8ec04fd4b854788fc020b311
c024 <c024@oa.fenri.com.tw>
2020-07-22 18:06:24 +0800

    article create mrthod, init article data, and modify footer and nav

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,10 +1,29 @@
 import article from './article.html';
 import './article.scss';
 
-function init() {
-  $('body').append($(article).contents());
+/**
+ * Create article block
+ * @param {Document} block The html bolck document tree whitch to append the article block
+ * @param {Object} data The article title、text and url link
+ */
+function create(block = $('body'), data) {
+  let articleBox = $(article).contents();
+  initArticleBox(articleBox, data)
+  .then( () => block.append(articleBox) )
+  .catch( error => console.log(error) );
+}
+
+/**
+ * Initial the article title、content text and button link
+ * @param {Document} dom The article template document tree
+ * @param {Object} data The article title、text and url link
+ */
+async function initArticleBox(dom, data) {
+  dom.find('#articleTitle').text(data.title);
+  dom.find('#articleText').text(data.text);
+  dom.find('#readMore').attr('href', data.link);
 }
 
 export default {
-  init: init
+  create: create
 }
--- src/js/home.js
+++ src/js/home.js
@@ -5,9 +5,23 @@ import Article from '../components/article/article.js';
 import Foot from '../components/foot/foot.js';
 import Nav from '../components/nav/nav.js';
 
-Nav.init();
-// $('body').append(
-//   '<div style="width: 100%; height: 200vh;"></div>'
-// );
-Article.init();
-Foot.init();
+function init() {
+  Nav.init();
+  $('body').append('<div id="view" class="home"></div>');
+  Foot.init();
+}
+
+function appendArticle() {
+  for(let i = 0; i < 5; i++) {
+    Article.create($('#view'), {
+      title: `Test Title-${i}`,
+      text: `學習當頁面往下滾動後如何讓 Navbar 黏著在視窗上方；其他當滾動滑鼠滾輪時可顯示、隱藏 Navbar ，最後當 Navbar 隱藏時透過監聽滑鼠行為，靠近時顯示及離開時隱藏。`,
+      link: `/test/${i}`
+    });
+  }
+}
+
+function getArticleList() {}
+
+init();
+appendArticle();
